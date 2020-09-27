2e29911ddb19db860b3649514327b3be1215ae93
Unknown <aymanbenali15@gmail.com>
2020-09-27 23:58:14 +0100

    Use Hooks instead of Classes

--- src/DialogCard.js
+++ src/DialogCard.js
@@ -15,61 +15,61 @@ const Transition = React.forwardRef(function Transition(props, ref) {
   return <Slide direction="up" ref={ref} {...props} />;
 });
 
-export default class DialogLog extends React.Component {
-  render() {
-    const {state, handle, data, screenSize} = this.props;
-    console.log(screenSize)
-    const { description, name, images, technologies } = data;
-    if (!description) return <div></div>;
-    const descriptions = description.split('.');
-    descriptions.pop();
-    const imgSize = screenSize.width > 1246 ? 300 : 280
+const DialogProject = (props) => {
+  const {state, handle, data, screenSize} = props;
+  console.log(screenSize)
+  const { description, name, images, technologies } = data;
+  if (!description) return <div></div>;
+  const descriptions = description.split('.');
+  descriptions.pop();
+  const imgSize = screenSize.width > 1246 ? 300 : 280
 
-    return (
-      <Dialog
-        fullScreen
-        open={state}
-        onClose={handle}
-        TransitionComponent={Transition}
-      >
-        <div style={{backgroundColor: '#FFFFFF'}}>
-          <AppBar>
-            <Toolbar>
-              <IconButton edge="start" color="inherit" onClick={handle} aria-label="close">
-                <CloseIcon />
-              </IconButton>
-              <Typography variant="h6">
-                {name}
-              </Typography>
-            </Toolbar>
-          </AppBar>
-          <div style={{
-              borderStyle: 'solid',
-              borderColor: '#7510F7',
-              borderRadius: '25px',
-              marginTop: '100px',
-              marginLeft: '20px',
-              marginRight: '20px',
-              padding: '30px'
-            }}>
-            <Grid container direction={screenSize.width < 1246 ? "column" : ''} justify="center" alignItems="stretch" >
-              <Grid item xs={3}>
-                <img alt="complex" src={images[0]} width={imgSize} height='200' />
-              </Grid>
-              <Grid item>
-                <Typography style={theme.palette.typographyWhiteColor}> {name} </Typography>
-                <Container maxWidth="la">
-                  {
-                    descriptions.map(description => <Typography style={theme.palette.typographyWhiteColor}> ✓{description}. </Typography>)
-                  }
-                </Container>
-              </Grid>
+  return (
+    <Dialog
+      fullScreen
+      open={state}
+      onClose={handle}
+      TransitionComponent={Transition}
+    >
+      <div style={{backgroundColor: '#FFFFFF'}}>
+        <AppBar>
+          <Toolbar>
+            <IconButton edge="start" color="inherit" onClick={handle} aria-label="close">
+              <CloseIcon />
+            </IconButton>
+            <Typography variant="h6">
+              {name}
+            </Typography>
+          </Toolbar>
+        </AppBar>
+        <div style={{
+            borderStyle: 'solid',
+            borderColor: '#7510F7',
+            borderRadius: '25px',
+            marginTop: '100px',
+            marginLeft: '20px',
+            marginRight: '20px',
+            padding: '30px'
+          }}>
+          <Grid container direction={screenSize.width < 1246 ? "column" : ''} justify="center" alignItems="stretch" >
+            <Grid item xs={3}>
+              <img alt="complex" src={images[0]} width={imgSize} height='200' />
             </Grid>
-            <br />
-            {generateChips(technologies)}
-          </div>          
-        </div>
-      </Dialog>
-    )
-  }
+            <Grid item>
+              <Typography style={theme.palette.typographyWhiteColor}> {name} </Typography>
+              <Container maxWidth="la">
+                {
+                  descriptions.map(description => <Typography style={theme.palette.typographyWhiteColor}> ✓{description}. </Typography>)
+                }
+              </Container>
+            </Grid>
+          </Grid>
+          <br />
+          {generateChips(technologies)}
+        </div>          
+      </div>
+    </Dialog>
+  )
 }
+
+export default DialogProject;
--- src/DialogContact.js
+++ src/DialogContact.js
@@ -15,80 +15,81 @@ const Transition = React.forwardRef(function Transition(props, ref) {
   return <Slide direction="up" ref={ref} {...props} />;
 });
 
-export default class DialogLog extends React.Component {
-  render() {
-    const {state, handle} = this.props;
-
-    return (
-      <Dialog
-        fullScreen
-        open={state}
-        onClose={handle}
-        TransitionComponent={Transition}
-      >
-        <div style={{backgroundColor: '#FFFFFF'}}>
-          <AppBar>
-            <Toolbar>
-              <IconButton edge="start" color="inherit" onClick={handle} aria-label="close">
-                <CloseIcon />
-              </IconButton>
-              <Typography variant="h6">
-                Contact me
-              </Typography>
-            </Toolbar>
-          </AppBar>
-          <div style={{
-              marginTop: '100px',
-              marginLeft: '20px',
-              marginRight: '20px',
-              padding: '30px'
-            }}>
-            <Grid container spacing={3}>
-              <Grid item xs={12}>
-                <Typography variant='h3' style={theme.palette.title}> Thanks for taking the time to reach out. How can I help you today?</Typography>
-              </Grid>
-              <Grid item xs={3} />
-              <Grid item xs={3}>
-                <TextField 
-                  id="outlined-secondary"
+const DialogContact = (props) => {
+  const {state, handle} = props; 
+  return (
+    <Dialog
+      fullScreen
+      open={state}
+      onClose={handle}
+      TransitionComponent={Transition}
+    >
+      <div style={{backgroundColor: '#FFFFFF'}}>
+        <AppBar>
+          <Toolbar>
+            <IconButton edge="start" color="inherit" onClick={handle} aria-label="close">
+              <CloseIcon />
+            </IconButton>
+            <Typography variant="h6">
+              Contact me
+            </Typography>
+          </Toolbar>
+        </AppBar>
+        <div style={{
+            marginTop: '100px',
+            marginLeft: '20px',
+            marginRight: '20px',
+            padding: '30px'
+          }}>
+          <Grid container spacing={3}>
+            <Grid item xs={12}>
+              <Typography variant='h3' style={theme.palette.title}> Thanks for taking the time to reach out. How can I help you today?</Typography>
+            </Grid>
+            <Grid item xs={3} />
+            <Grid item xs={3}>
+              <TextField 
+                id="outlined-secondary"
+                label="Outlined secondary"
+                variant="outlined"
+                label="Name" />
+            </Grid>
+            <Grid item xs={3}>
+              <TextField
+                id="outlined-secondary"
+                label="Outlined secondary"
+                variant="outlined"
+                label="Email" />
+            </Grid>
+            <Grid item xs={3} />
+            <Grid item xs={3} />
+            <Grid item>
+              <TextField
+                id="outlined-multiline-static"
+                label="Multiline"
+                multiline
+                rows={14}
+                style={{width: '226%'}}
+                variant="outlined"
+              />
+            </Grid>
+            <Grid container justify="center" alignItems="stretch" >
+              <Grid item xs={2} />
+              <Grid item xs={4}>
+                <Button
                   variant="outlined"
-                  label="Name" />
-              </Grid>
-              <Grid item xs={3}>
-                <TextField
-                  id="outlined-secondary"
-                  variant="outlined"
-                  label="Email" />
-              </Grid>
-              <Grid item xs={3} />
-              <Grid item xs={3} />
-              <Grid item>
-                <TextField
-                  id="outlined-multiline-static"
-                  label="Multiline"
-                  multiline
-                  rows={14}
-                  style={{width: '226%'}}
-                  variant="outlined"
-                />
-              </Grid>
-              <Grid container justify="center" alignItems="stretch" >
-                <Grid item xs={2} />
-                <Grid item xs={4}>
-                  <Button
-                    variant="outlined"
-                    color="primary"
-                    style={{borderStyle: 'dotted', borderWidth: '5px', borderRadius: '25px'}}
-                  >
-                    Submit
-                  </Button> 
-                </Grid>
+                  color="primary"
+                  style={{borderStyle: 'dotted', borderWidth: '5px', borderRadius: '25px'}}
+                >
+                  Submit
+                </Button> 
               </Grid>
             </Grid>
-            <br />
-          </div>          
-        </div>
-      </Dialog>
-    )
-  }
-}
+          </Grid>
+          <br />
+        </div>          
+      </div>
+    </Dialog>
+  )
+};
+
+export default DialogContact;
