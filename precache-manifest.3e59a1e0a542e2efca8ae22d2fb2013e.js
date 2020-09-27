d495a44c3e92fe7524c15c6578e298520a5847e6
ayman benali <aymanbenali15@gmail.com>
2020-09-27 13:26:18 +0100

    Updates

---
+++ precache-manifest.3e59a1e0a542e2efca8ae22d2fb2013e.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8072264258f3d155cc71d16a460df2c8",
+    "url": "/portfolio/index.html"
+  },
+  {
+    "revision": "69bebe58d51cb8a69899",
+    "url": "/portfolio/static/css/main.06c988bc.chunk.css"
+  },
+  {
+    "revision": "33e1488820d28a6f085f",
+    "url": "/portfolio/static/js/2.b1a9d1e9.chunk.js"
+  },
+  {
+    "revision": "d683f7fda08bbbfccf5e70b48924783e",
+    "url": "/portfolio/static/js/2.b1a9d1e9.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "69bebe58d51cb8a69899",
+    "url": "/portfolio/static/js/main.77582ebe.chunk.js"
+  },
+  {
+    "revision": "7a6cc99bd8f08f9fa6ec",
+    "url": "/portfolio/static/js/runtime-main.209c2d7e.js"
+  },
+  {
+    "revision": "bd5337189e3748a8a8ac6d3f8e1be8c4",
+    "url": "/portfolio/static/media/Ayman.bd533718.png"
+  },
+  {
+    "revision": "df1e2f2c0739840c615d053c338e9e65",
+    "url": "/portfolio/static/media/quoteWhite.df1e2f2c.png"
+  }
+]);
