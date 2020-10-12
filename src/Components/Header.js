9822b150a2bae491b7ae364df012603ce284f31c
Unknown <aymanbenali15@gmail.com>
2020-10-12 23:08:46 +0100

    Add cv into header

--- src/Components/Header.js
+++ src/Components/Header.js
@@ -1,9 +1,9 @@
-import React from 'react';
-import DialogContact from './subComponents/DialogContact';
-import Grid from '@material-ui/core/Grid';
-import Button from '@material-ui/core/Button';
-
-const Header = ({theme}) => {
+import React from "react";
+import DialogContact from "./subComponents/DialogContact";
+import Grid from "@material-ui/core/Grid";
+import Button from "@material-ui/core/Button";
+import pdf from "../data/Ayman_CV_Eng.pdf";
+const Header = ({ theme }) => {
   const [open, setOpen] = React.useState(false);
 
   const handleClickOpen = () => {
@@ -15,23 +15,41 @@ const Header = ({theme}) => {
   };
 
   return (
-    <div style={{marginTop: '20px'}}>
-      <DialogContact state={open} handle={handleClose} titleTheme={theme}/>
-      <Grid container justify="center" alignItems="stretch" >
-        <Grid item xs={10} />
+    <div style={{ marginTop: "20px" }}>
+      <DialogContact state={open} handle={handleClose} titleTheme={theme} />
+      <Grid container justify="center" alignItems="stretch">
         <Grid item xs={2}>
           <Button
             variant="outlined"
             color="primary"
-            style={{borderStyle: 'dotted', borderWidth: '5px', borderRadius: '25px'}}
+            style={{
+              borderStyle: "dotted",
+              borderWidth: "5px",
+              borderRadius: "25px",
+            }}
+            href={pdf}
+          >
+            Resume
+          </Button>
+        </Grid>
+        <Grid item xs={8} />
+        <Grid item xs={2}>
+          <Button
+            variant="outlined"
+            color="primary"
+            style={{
+              borderStyle: "dotted",
+              borderWidth: "5px",
+              borderRadius: "25px",
+            }}
             onClick={handleClickOpen}
           >
             Say Hello
-          </Button>      
+          </Button>
         </Grid>
       </Grid>
     </div>
-  )
-}
+  );
+};
 
 export default Header;
